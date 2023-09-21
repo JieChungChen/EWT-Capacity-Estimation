457d9ea20e0cb3b180aaed0dacea5e5954f0e9aa
JieChungChen <jackson29.en11@nycu.edu.tw>
2023-09-21 14:55:09 +0800

    Update load_Stanford_data.py

--- load_Stanford_data.py
+++ load_Stanford_data.py
@@ -36,7 +36,7 @@ def mat_to_npy(folder='Stanford_Dataset', sep=10):
     sep: int, 採樣間隔
     """
     # 去除有問題的cycle
-    ignore_dict = {'G1':[29], 'V4':[69], 'V5':[], 'W4':[], 'W5':[192], 'W8':[], 'W9':[137], 'W10':[]}
+    ignore_dict = {'G1':[29], 'V4':[69], 'V5':[], 'W4':[], 'W5':[192], 'W8':[], 'W9':[137, 166], 'W10':[]}
     for cell_id in CELL_IDS:
         dc_v = []
         for cycle in range(14):
@@ -57,24 +57,8 @@ def mat_to_npy(folder='Stanford_Dataset', sep=10):
                 os.makedirs('%s/discharge_info/%s'%(folder, cell_id), exist_ok=True)
                 np.save('%s/discharge_info/%s/cycle_%s.npy'%(folder, cell_id, str(i+1).zfill(3)), cycle_info[points])
 
-
-def data_visualization(folder, cell_id):
-    files = os.listdir(folder+'/'+cell_id)
-    cmap = plt.get_cmap('coolwarm')
-    for i, f in enumerate(files):
-        v_curve = np.load(folder+'/'+cell_id+'/'+f)
-        points = np.arange(0, len(v_curve), 10)
-        plt.plot(points, v_curve[points], c=cmap(i/len(files)), alpha=0.7, lw=0.5)
-        # plt.savefig('figs/%d.png'%(i+1))
-    sm = plt.cm.ScalarMappable(norm=plt.Normalize(vmin=1, vmax=len(files)),cmap='coolwarm')
-    sm.set_array([])
-    plt.colorbar(sm)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Voltage (V)')
-    plt.show()
     
     
-
 def feature_extraction(v):
     v_abs = np.abs(v)
 
@@ -101,7 +85,7 @@ def npy_preprocessing(folder):
         feature_list = []
         for f in files:
             v_t = np.load(folder+'/'+cell_id+'/'+f)
-            v_t = v_t[10000:46000, 0]
+            v_t = v_t[0:36000, 0]
             ewt, mfb ,boundaries = EWT1D(v_t, N=9)
             corr = np.array([np.corrcoef(ewt[:, mode], v_t)[0, 1] for mode in range(9)])
             f_mode = ewt[:, np.argmax(corr)]
@@ -120,11 +104,66 @@ def npy_preprocessing(folder):
         plt.show()
         plt.close()
 
+
+def v_curve_visualization(folder, cell_id):
+    files = os.listdir(folder+'/'+cell_id)
+    cmap = plt.get_cmap('coolwarm')
+    for i, f in enumerate(files):
+        v_curve = np.load(folder+'/'+cell_id+'/'+f)
+        points = np.arange(0, len(v_curve), 10)
+        plt.plot(points, v_curve[points], c=cmap(i/len(files)), alpha=0.7, lw=0.5)
+        # plt.savefig('figs/%d.png'%(i+1))
+    sm = plt.cm.ScalarMappable(norm=plt.Normalize(vmin=1, vmax=len(files)),cmap='coolwarm')
+    sm.set_array([])
+    plt.colorbar(sm)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Voltage (V)')
+    plt.show()
+    plt.close()
+
+
+def ewt_visualization():
+    folder = 'Stanford_Dataset/discharge_info/W8'
+    files = os.listdir(folder)
+    v_t = np.load(folder+'/'+files[0])
+    ewt, mfb ,boundaries = EWT1D(v_t[:, 0], N=9)
+
+    fig, ax = plt.subplots(3, 3)
+
+    for i in range(9):
+        ax[i//3,i%3].plot(np.arange(len(v_t)), ewt[:, i])
+        ax[i//3,i%3].set_title('Mode%d'%(i+1))
+        ax[i//3,i%3].set_xlabel('Time (s)', fontsize=8)
+        ax[i//3,i%3].set_ylabel('Voltage (V)', fontsize=8)
+    plt.tight_layout()
+    plt.show()
+    plt.close()
     
+    # corr = np.array([np.corrcoef(ewt[:, mode], v_t[:, 0])[0, 1] for mode in range(9)])
+    f_mode = ewt[:, 0]+ewt[:, 1]
+    v_c = v_t[:,0]-f_mode
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(v_t, c='blue', ls='--', label='Voltage') # original signal
+    ax[0].plot(f_mode, c='red', label='Mode 1') # fundamental mode
+    ax[0].set_xlabel('Time(s)', fontsize=12)
+    ax[0].set_ylabel('Voltage (V)', fontsize=12)
+    ax[0].set_title('original voltage signal', fontsize=14)
+    ax[0].legend()
+
+    ax[1].plot(v_c, c='blue')
+    ax[1].set_xlabel('Time(s)', fontsize=12)
+    ax[1].set_ylabel('Voltage (V)', fontsize=12)
+    ax[1].set_title('filtered signal', fontsize=14)
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+
 def main():
     # mat_to_npy('Stanford_Dataset')
-    data_visualization('Stanford_Dataset/discharge_info', 'V5')
-    # npy_preprocessing('Stanford_Dataset/discharge_info')
+    # v_curve_visualization('Stanford_Dataset/discharge_info', 'W9')
+    # ewt_visualization()
+    npy_preprocessing('Stanford_Dataset/discharge_info')
 
 
 if __name__ == '__main__':
