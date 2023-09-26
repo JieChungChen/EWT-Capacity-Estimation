bef5bd35557ac7fd9f8530227101240f10178318
JieChungChen <jackson29.en11@nycu.edu.tw>
2023-09-26 13:56:46 +0800

    Update load_Stanford_data.py

--- load_Stanford_data.py
+++ load_Stanford_data.py
@@ -26,7 +26,7 @@ def get_mat_voltage(filepath, plot=False):
         plt.legend()
         plt.show()
         plt.close()
-    return voltage, step
+    return voltage, current, step
 
 
 def mat_to_npy(folder='Stanford_Dataset', sep=10):
@@ -36,22 +36,25 @@ def mat_to_npy(folder='Stanford_Dataset', sep=10):
     sep: int, 採樣間隔
     """
     # 去除有問題的cycle
-    ignore_dict = {'G1':[29], 'V4':[69], 'V5':[], 'W4':[], 'W5':[192], 'W8':[], 'W9':[137, 166], 'W10':[]}
+    ignore_dict = {'G1':[29], 'V4':[69], 'V5':[], 'W4':[], 'W5':[192], 'W8':[], 'W9':[137, 167], 'W10':[]}
     for cell_id in CELL_IDS:
-        dc_v = []
+        dc_v, dc_c = [], []
         for cycle in range(14):
             filepath = '%s/Cycling_%d/%s.mat' % (folder, cycle+1, cell_id)
             if os.path.exists(filepath):
-                v, s = get_mat_voltage(filepath)
+                v, c, s = get_mat_voltage(filepath)
                 v[s!=14] = 0 # remove non-discharging part
                 dif = np.abs(np.diff(v, axis=0)) # calculate discharge boundaries
                 edge = np.argwhere(dif>1)+1 # find discharge boundaries
                 dc_v+=[v[edge[i, 0]:edge[i+1, 0]] for i in range(0, len(edge)-1, 2)] # separate discharge area, len(edge)-1 for odd number boundaries
+                dc_c+=[c[edge[i, 0]:edge[i+1, 0]] for i in range(0, len(edge)-1, 2)]
                 print('%s partial discharge segments=%d'%(cell_id, len(edge)//2))
         print('%s total discharge segments=%d'%(cell_id, len(dc_v)))
 
-        for i, cycle_info in enumerate(dc_v):
-            # cycle_info shape: (time_step, 1)
+        for i in range(len(dc_v)):
+            # cycle_info shape: (time_step, 2)
+            v, c = dc_v[i], dc_c[i]
+            cycle_info = np.hstack([v, c])
             if (i+1) not in ignore_dict[cell_id]:
                 points = np.arange(0, len(cycle_info), sep)
                 os.makedirs('%s/discharge_info/%s'%(folder, cell_id), exist_ok=True)
@@ -75,6 +78,19 @@ def feature_extraction(v):
     return [cl_f, cr_f, ip_f, kur, m, v_peak, rms, sh_f, skew, sd]
 
 
+def npy_filtering(folder):
+    cell_id_list = os.listdir(folder+'/discharge_info')
+    for cell_id in cell_id_list:
+        os.makedirs('%s/filtered_info/%s'%(folder, cell_id), exist_ok=True)
+        files = os.listdir(folder+'/discharge_info/'+cell_id)
+        for i, f in enumerate(files):
+            v_t = np.load(folder+'/discharge_info/'+cell_id+'/'+f)
+            ewt, mfb ,boundaries = EWT1D(v_t[:, 0], N=9)
+            f_mode = ewt[:, 0]
+            v_c = v_t[:,0]-f_mode
+            np.save('%s/filtered_info/%s/cycle_%s.npy'%(folder, cell_id, str(i+1).zfill(3)), v_c)
+
+
 def npy_to_selected_features(folder):
     feature_names = ['Clearance Factor', 'Crest Factor', 'Impulse Factor', 'Kurtosis', 'Mean', 'Peak Value', 'RMS', 'Shape Factor', 'Skewness', 'SD']
     cell_id_list = os.listdir(folder)
@@ -84,16 +100,15 @@ def npy_to_selected_features(folder):
         feature_list = []
         for f in files:
             v_t = np.load(folder+'/'+cell_id+'/'+f)
-            v_t = v_t[0:36000, 0]
+            start = np.random.randint(0, len(v_t)-3600, size=1)[0]
+            v_t = v_t[start:start+3600, 0]
             ewt, mfb ,boundaries = EWT1D(v_t, N=9)
-            corr = np.array([np.corrcoef(ewt[:, mode], v_t)[0, 1] for mode in range(9)])
-            f_mode = ewt[:, np.argmax(corr)]
-            v_c = v_t-f_mode 
+            v_c = v_t-ewt[:, 0] 
             feature_list.append(feature_extraction(v_c))
         feature_list = np.vstack(feature_list)
         fig, ax = plt.subplots(2, 5, figsize=(12, 6))
         for i in range(10):
-            ax[i//5,i%5].plot(feature_list[:, i])
+            ax[i//5,i%5].plot(feature_list[:, i], lw=1)
             ax[i//5,i%5].set_title(feature_names[i], fontsize=8)
             ax[i//5,i%5].tick_params(axis='x', labelsize=5)
             ax[i//5,i%5].tick_params(axis='y', labelsize=5)
@@ -104,21 +119,33 @@ def npy_to_selected_features(folder):
         plt.close()
 
 
-def v_curve_visualization(folder, cell_id):
+def cycle_info_visualization(folder, cell_id, mode='full'):
     files = os.listdir(folder+'/'+cell_id)
     cmap = plt.get_cmap('coolwarm')
     for i, f in enumerate(files):
-        v_curve = np.load(folder+'/'+cell_id+'/'+f)
-        points = np.arange(0, len(v_curve), 10)
-        plt.plot(points, v_curve[points], c=cmap(i/len(files)), alpha=0.7, lw=0.5)
+        cycle_info = np.load(folder+'/'+cell_id+'/'+f)
+        if mode=='full':
+            plt.plot(cycle_info[:, 1], c=cmap(i/len(files)), alpha=0.7, lw=0.5, label='current')
+        elif mode=='partial':
+            fig, ax1 = plt.subplots()
+            plt.xlabel('Cycles')
+            ax2 = ax1.twinx()
+            ax1.plot(cycle_info[:3600, 0], c='blue', alpha=0.7, lw=0.5, label='voltage')
+            ax2.plot(cycle_info[:3600, 1], c='red', alpha=0.7, lw=0.5, label='current')
+            ax1.set_ylabel('Voltage (V)')
+            ax2.set_ylabel('Current (I)')
+            plt.legend()
+            plt.show()
+            plt.close()
         # plt.savefig('figs/%d.png'%(i+1))
-    sm = plt.cm.ScalarMappable(norm=plt.Normalize(vmin=1, vmax=len(files)),cmap='coolwarm')
-    sm.set_array([])
-    plt.colorbar(sm)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Voltage (V)')
-    plt.show()
-    plt.close()
+    if mode=='full':
+        sm = plt.cm.ScalarMappable(norm=plt.Normalize(vmin=1, vmax=len(files)),cmap='coolwarm')
+        sm.set_array([])
+        plt.colorbar(sm)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Voltage (V)')
+        plt.show()
+        plt.close()
 
 
 def ewt_visualization():
@@ -160,9 +187,11 @@ def ewt_visualization():
 
 def main():
     # mat_to_npy('Stanford_Dataset')
-    # v_curve_visualization('Stanford_Dataset/discharge_info', 'W9')
+    # for cell in CELL_IDS:
+    #     cycle_info_visualization('Stanford_Dataset/discharge_info', cell)
+    npy_filtering('Stanford_Dataset')
     # ewt_visualization()
-    npy_to_selected_features('Stanford_Dataset/discharge_info')
+    # npy_to_selected_features('Stanford_Dataset/discharge_info')
 
 
 if __name__ == '__main__':
